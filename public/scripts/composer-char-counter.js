d92d822220492be1fab4746b8d5acf0f311b5aeb
Xavier Martin Demers <xavier.pmd@outlook.com>
2021-10-10 01:37:33 -0400

    Performed another refactor of the css files in the styles folder so that the paths to every element weren't as long an confusing. Also added the all tweets section and the corresponding css.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-  $('#tweet-text').keyup(function() {
-    let textCount = 140 - $(this).val().length;
-    $('#tweet-counter').text(textCount);
-    if (textCount >= 0) {
-      $('#tweet-counter').removeClass('negative');
-      $('#tweet-counter').addClass('positive');
-    } else if (textCount < 0) {
-      $('#tweet-counter').removeClass('positive');
-      $('#tweet-counter').addClass('negative');
+  $('section.new-tweet form textarea').keyup(function() {
+    const charCounter = $(this).siblings('div.add-ons').children('output');
+    const textCount = 140 - $(this).val().length;
+    charCounter.text(textCount);
+    if (textCount < 0) {
+      charCounter.addClass('negative');
+    } else {
+      charCounter.removeClass('negative');
     }
   });
 });
